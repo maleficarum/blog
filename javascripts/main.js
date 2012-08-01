3d5add94832a3cc71767aea3578e1487e5f2eef4
Oscar Ivan Hernandez Ventura <oscar@angellore.com.mx>
2012-07-31 22:11:47 -0500

    Added jalarms 1 page

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,18 @@
-console.log('This would be the main JS file.');
+var MALEFICARUM = {
+	Utils : {
+		loadPages: function(element) {
+
+			var pages = {
+				"jAlarms [ Multiples origenes ]": "jalarms1.html"
+			};
+
+			var menu = "";
+			
+			$.each(pages, function(a,b) {
+				menu += "<li><a href='" + b + "'>" + a + "</a></li>";
+			});
+
+			$(element).html(menu);
+		}
+	}
+}
